0519b2ac468dfce708ca30bca5afc95174ecee58
M. Farhan Mahmood <farhanmahmood433@gmail.com>
2022-05-28 02:06:34 +0500

    Initial Setup

---
+++ README.md
@@ -0,0 +1,22 @@
+# project-planner
+The project planner Vue App from Net Ninja Udemy Course.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Start the JSON Server for the application
+```
+npm run watch-db
+```
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+body {
+  background: #f2f2f2;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+}
+</style>
